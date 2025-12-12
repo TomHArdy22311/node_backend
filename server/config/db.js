e4606a2250cc99326e46a81ffd18675f9693dd69
TomHArdy22311 <karanbangar255@gmail.com>
2025-12-13 01:34:57 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose')
-const dbPath = 'mongodb+srv://karanb:Gandhi_42069@cluster0.qvzb7bx.mongodb.net/kizaTexttiles'
+const dbPath = 'mongodb+srv://@cluster0.qvzb7bx.mongodb.net/kizaTexttiles'
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(dbPath, options).then(res => {
     console.log("Db Connected")
