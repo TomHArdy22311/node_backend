09ebd27a21c1f2f72ad92b0ca73361abdd01e6f4
TomHArdy22311 <karanbangar255@gmail.com>
2026-02-25 21:07:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/server/public/'));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false, parameterLimit: 500000000 }));
 app.get('/', (req, res) => {
-    res.send('hey guys!!')
+    res.send('I have to finish MY Story!!')
 })
 
 app.use('/api', apiRoutes)
